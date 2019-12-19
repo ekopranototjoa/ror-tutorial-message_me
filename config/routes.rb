e41cdb860e9bec6e6ad03e42b143e977f04c0763
Eko Pranoto Tjoa <EkoPranoto.Tjoa@careerbuilder.com>
2019-12-19 15:46:38 +0800

    add root and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'chat_room#index'
+  get 'login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
